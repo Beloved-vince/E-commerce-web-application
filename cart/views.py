95c4f997945c5b80ba6c6c819a4bbee03250454e
Beloved-vince <vinceoludare@gmail.com>
2023-06-15 11:50:32 +0100

    carting

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from main.models import Cart, CartItem
+from .forms import AddToCartForm
 
 # Create your views here.
+
+class CartView(View):
+    def get(self, request):
+        if request.user.is_authenticated:
+            cart = Cart.objects.get(user=request.user)
+            cart_items = CartItem.objects.filter(cart=cart)
+        else:
+            cart_items = []
+    
+        total_price = sum(items)
