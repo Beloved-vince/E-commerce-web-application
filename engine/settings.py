2898d0ac492ba23c4fce46eb2e456e0d11ab4625
Beloved-vince <vinceoludare@gmail.com>
2023-07-14 15:28:39 +0100

    email-confirmation fixed

--- engine/settings.py
+++ engine/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-i^)p7c%pn2v$n-ln3l#by56(xte54fb_65w770*kmr@a3eu)a-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 SITE_ID = 1
 
